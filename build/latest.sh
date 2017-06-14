845a53a5b833ad8d6065d0bb39db6339b781611e
Smirnov Artem <urpylka@gmail.com>
2017-06-14 17:25:56 +0400

    дебаг

--- build/latest.sh
+++ build/latest.sh
@@ -4,4 +4,5 @@ rm -rf autocopter
 git clone https://github.com/urpylka/autocopter.git
 chmod +x /home/pi/autocopter/build/*
 chmod +x /home/pi/autocopter/autocopter.py
+# FOR DEBUG
 #python autocopter/autocopter.py 'TOKEN'
---
+++ build/rssh2.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+ssh -q -N -R 14550:localhost:14550 -i /home/pi/.ssh/id_rsa urpylka@smirart.ru -p32122
