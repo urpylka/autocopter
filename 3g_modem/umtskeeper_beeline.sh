0c275368134ac11fd39089f4457a1a869e6f0821
Smirnov Artem <urpylka@gmail.com>
2017-06-14 17:53:20 +0400

    дебаг

--- 3g_modem/umtskeeper_beeline.sh
+++ 3g_modem/umtskeeper_beeline.sh
@@ -4,5 +4,5 @@ echo "starting umtskeeper"
 #по-хорошему нужно перенести данный скрипт в тело демона
 #прописаны параметры подключения к сети билайн и и мегафон модему E352 (кажется), перепрошитому утилитой Huawei
 #специально отключил --silent тк в init.d есть параметр --background и перенаправление потока ввода-выводы > /dev/null
-/home/pi/3g_modem/umtskeeper --sakisoperators "USBINTERFACE='0' OTHER='USBMODEM' USBMODEM='12d1:1506' APN='CUSTOM_APN' CUSTOM_APN='internet.beeline.ru' SIM_PIN='0000' APN_USER='beeline' APN_PASS='beeline'" --sakisswitches "--sudo --console" --devicename 'Huawei' --log --nat 'no'
+/home/pi/autocopter/3g_modem/umtskeeper --sakisoperators "USBINTERFACE='0' OTHER='USBMODEM' USBMODEM='12d1:1506' APN='CUSTOM_APN' CUSTOM_APN='internet.beeline.ru' SIM_PIN='0000' APN_USER='beeline' APN_PASS='beeline'" --sakisswitches "--sudo --console" --devicename 'Huawei' --log --nat 'no'
 
