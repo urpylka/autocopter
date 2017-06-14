748ac4e20f1a80de02018003954adbcf46b8bbc8
Smirnov Artem <urpylka@gmail.com>
2017-06-14 17:37:21 +0400

    дебаг

--- build/latest.sh
+++ build/latest.sh
@@ -2,7 +2,25 @@
 cd /home/pi
 rm -rf autocopter
 git clone https://github.com/urpylka/autocopter.git
-chmod +x /home/pi/autocopter/build/*
-chmod +x /home/pi/autocopter/autocopter.py
+
+chmod +x autocopter/build/*
+chmod +x autocopter/autocopter.py
+
+cp -f autocopter/daemons/umtskeeper /etc/init.d/umtskeeper
+chmod +x /etc/init.d/umtskeeper
+chown root:root /etc/init.d/umtskeeper
+update-rc.d umtskeeper defaults
+
+cp -f autocopter/daemons/mavgateway /etc/init.d/mavgateway
+chmod +x /etc/init.d/mavgateway
+chown root:root /etc/init.d/mavgateway
+update-rc.d mavgateway defaults
+
+cp -f autocopter/daemons/autocopter /etc/init.d/autocopter
+chmod +x /etc/init.d/autocopter
+chown root:root /etc/init.d/autocopter
+update-rc.d autocopter defaults
+
+
 # FOR DEBUG
 #python autocopter/autocopter.py 'TOKEN'
--- build/rssh2.sh
+++ build/rssh2.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ssh -q -N -R 14550:localhost:14550 -i /home/pi/.ssh/id_rsa urpylka@smirart.ru -p32122
+ssh -q -N -R 5760:localhost:5760 -i /home/pi/.ssh/id_rsa urpylka@smirart.ru -p32122
