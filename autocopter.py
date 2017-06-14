a7dd7738c6a1be4fc53e8de7de5cf136879940cc
Smirnov Artem <urpylka@gmail.com>
2017-06-14 15:52:15 +0400

    дебаг

--- autocopter.py
+++ autocopter.py
@@ -12,6 +12,11 @@ sys.setdefaultencoding('utf8')
 from other_functions import wait_internet
 wait_internet()
 
+# https://ru.stackoverflow.com/questions/225896/%D0%97%D0%B0%D0%BF%D1%83%D1%81%D0%BA-bash-%D0%B8%D0%B7-%D1%81%D0%BA%D1%80%D0%B8%D0%BF%D1%82%D0%B0-python-2-7-3
+import subprocess
+p = subprocess.Popen(['/home/pi/rssh.sh'], stdout=subprocess.PIPE)
+#line = p.stdout.readline()
+
 STATE = 'INIT'
 try:
     # START TELEGRAM BOT
