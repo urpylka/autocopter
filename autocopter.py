52a0a146271f2786c9055544e3f879cf1e769d86
Smirnov Artem <urpylka@gmail.com>
2017-06-15 18:04:29 +0400

    дебаг

--- autocopter.py
+++ autocopter.py
@@ -24,7 +24,6 @@ try:
             # ==========================================================================================================
             log_and_messages = log_and_messages(bot)
             bot.sendMessage(62922848, 'msg')
-            log_and_messages.deb_pr_tel("Autocopter is online:")
             #log_and_messages.deb_pr_tel("Autocopter is online: %s" % get_ip())
 
             def handle(msg):
