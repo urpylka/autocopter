1d8dc04a8d214c812934d97b0d35301138e06e44
Smirnov Artem <urpylka@gmail.com>
2017-06-16 18:22:06 +0400

    новая структура

--- rssh.py
+++ rssh.py
@@ -8,20 +8,27 @@ print 'internet'
 # ==========================================================================================================
 # https://ru.stackoverflow.com/questions/225896/%D0%97%D0%B0%D0%BF%D1%83%D1%81%D0%BA-bash-%D0%B8%D0%B7-%D1%81%D0%BA%D1%80%D0%B8%D0%BF%D1%82%D0%B0-python-2-7-3
 import subprocess, time
-while True:
-    try:
-        # https://pythonworld.ru/moduli/modul-subprocess.html
-        p = subprocess.Popen(['/home/pi/autocopter/build/rssh.sh'], subprocess.PIPE)
-        print 'smirart.ru:2202'
-        p2 = subprocess.Popen(['/home/pi/autocopter/build/rssh2.sh'], subprocess.PIPE)
-        print 'smirart.ru:5760'
-        # потом переделать под это https://pythonworld.ru/moduli/modul-subprocess.html
-        # ==========================================================================================================
-        while True:
-            time.sleep(10)
-    except Exception as ex:
-        print ex.message + '\n'
-        time.sleep(4)
-    finally:
-        p.kill()
-        p2.kill()
+
+try:
+    while True:
+        try:
+            # https://pythonworld.ru/moduli/modul-subprocess.html
+            p = subprocess.Popen(['/home/pi/autocopter/build/rssh.sh'], subprocess.PIPE)
+            print 'smirart.ru:2202'
+            p2 = subprocess.Popen(['/home/pi/autocopter/build/rssh2.sh'], subprocess.PIPE)
+            print 'smirart.ru:5760'
+            # потом переделать под это https://pythonworld.ru/moduli/modul-subprocess.html
+            # ==========================================================================================================
+            while True:
+                time.sleep(10)
+        except Exception as ex:
+            print ex.message + '\n'
+            time.sleep(4)
+        finally:
+            # https://pythonworld.ru/moduli/modul-subprocess.html
+            p.kill()
+            p2.kill()
+finally:
+    # https://pythonworld.ru/moduli/modul-subprocess.html
+    p.kill()
+    p2.kill()
