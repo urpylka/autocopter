4df44e532e9d8749a02ea3fac17df9cb8ba8184e
Smirnov Artem <urpylka@gmail.com>
2017-06-16 19:26:58 +0400

    новая структура

--- rssh.py
+++ rssh.py
@@ -8,32 +8,23 @@ print 'internet'
 # ==========================================================================================================
 # https://ru.stackoverflow.com/questions/225896/%D0%97%D0%B0%D0%BF%D1%83%D1%81%D0%BA-bash-%D0%B8%D0%B7-%D1%81%D0%BA%D1%80%D0%B8%D0%BF%D1%82%D0%B0-python-2-7-3
 import subprocess, time
-
 try:
-    while True:
-        try:
-            # https://pythonworld.ru/moduli/modul-subprocess.html
-            with subprocess.Popen(['/home/pi/autocopter/build/rssh.sh'], stdout=subprocess.PIPE) as proc:
-                print "ff"+proc.stdout.read()
-
-            #p = subprocess.Popen(['/home/pi/autocopter/build/rssh.sh'], subprocess.PIPE)
-            print 'smirart.ru:2202'
-            #p2 = subprocess.Popen(['/home/pi/autocopter/build/rssh2.sh'], subprocess.PIPE)
-            #print 'smirart.ru:5760'
-            # потом переделать под это https://pythonworld.ru/moduli/modul-subprocess.html
-            # ==========================================================================================================
-            while True:
-                time.sleep(10)
-        except Exception as ex:
-            print ex.message + '\n'
-            time.sleep(4)
-        finally:
-            print "first final"
-            # https://pythonworld.ru/moduli/modul-subprocess.html
-            #p.kill()
-            #p2.kill()
-finally:
     # https://pythonworld.ru/moduli/modul-subprocess.html
-    print "second final"
-    #p.kill()
-    #p2.kill()
+    p = subprocess.Popen(['/home/pi/autocopter/build/rssh.sh'], subprocess.PIPE)
+    print 'smirart.ru:2202'
+    p2 = subprocess.Popen(['/home/pi/autocopter/build/rssh2.sh'], subprocess.PIPE)
+    print 'smirart.ru:5760'
+    if p.stdout != None or p2.stdout != None:
+        raise Exception("что-то пошло не так")
+    # потом переделать под это https://pythonworld.ru/moduli/modul-subprocess.html
+    # ==========================================================================================================
+    while True:
+        time.sleep(10)
+except Exception as ex:
+    print ex.message + '\n'
+    time.sleep(4)
+finally:
+    print "first final"
+    # https://pythonworld.ru/moduli/modul-subprocess.html
+    p.kill()
+    p2.kill()
