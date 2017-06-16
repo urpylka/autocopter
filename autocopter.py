2d9ae227ea502a37b31cc6fd6afc579033b19882
Smirnov Artem <urpylka@gmail.com>
2017-06-16 14:32:27 +0400

    новая структура

--- autocopter.py
+++ autocopter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
-from log_and_messages import *
+from lam import *
 import sys, telepot, time, traceback
 from dronekit_functions import *
 # ====================================================================================
@@ -27,8 +27,9 @@ try:
                 TOKEN = sys.argv[1]  # get token from command-line
                 bot = telepot.Bot(TOKEN)
                 # ==========================================================================================================
-                log_and_messages = log_and_messages(bot, MY_CHAT_ID, DEBUG)
-                log_and_messages.deb_pr_tel("Autocopter is online: %s" % get_ip())
+                from log_and_messages import *
+                lam = log_and_messages(bot, MY_CHAT_ID, DEBUG)
+                lam.deb_pr_tel("Autocopter is online: %s" % get_ip())
 
                 def handle(msg):
                     """хендлер выполняется в отдельном потоке, вызывается событием на подобие блокирующей очереди"""
@@ -37,92 +38,92 @@ try:
                         print(content_type, chat_type, chat_id)
                     if chat_id == MY_CHAT_ID:
                         if content_type == 'text':
-                            log_and_messages.deb_pr_tel(dronekit.new_command(STATE, msg['text']))
+                            lam.deb_pr_tel(dronekit.new_command(STATE, msg['text']))
                         elif content_type == 'location':
-                            log_and_messages.deb_pr_tel(dronekit.new_command(STATE, 'create_mission', msg['location']))
+                            lam.deb_pr_tel(dronekit.new_command(STATE, 'create_mission', msg['location']))
                         else:
-                            log_and_messages.deb_pr_tel('Ошибка 2! Неверный тип: только text и location')
+                            lam.deb_pr_tel('Ошибка 2! Неверный тип: только text и location')
                     else:
-                        log_and_messages.deb_pr_tel('Ошибка 1! Access error!')
+                        lam.deb_pr_tel('Ошибка 1! Access error!')
                 bot.message_loop(handle)
                 time.sleep(1.5)  # время на ответ сообщений пришедших в выключенный период
 
-                log_and_messages.deb_pr_tel('Connecting to APM ...')
-                dronekit = autocopterDronekit(log_and_messages.deb_pr_tel)  # для доступности в finally (ВРОДЕ КАК НЕ НАДО)
+                lam.deb_pr_tel('Connecting to APM ...')
+                dronekit = autocopterDronekit(lam.deb_pr_tel)  # для доступности в finally (ВРОДЕ КАК НЕ НАДО)
                 if dronekit.status_of_connect:
                     nextState = 'IDLE'
-                    log_and_messages.deb_pr_tel('Connect successful!\nListening ...')
-                log_and_messages.deb_pr_tel(dronekit.status())
+                    lam.deb_pr_tel('Connect successful!\nListening ...')
+                lam.deb_pr_tel(dronekit.status())
                     # Keep the program running.
             except BaseException as ex:
-                log_and_messages.deb_pr_tel('Произошла ошибка:\n' + ex.message + "\n" + traceback.format_exc() + '\nв состоянии ' + STATE + ', переход в состояние ' + nextState)
+                lam.deb_pr_tel('Произошла ошибка:\n' + ex.message + "\n" + traceback.format_exc() + '\nв состоянии ' + STATE + ', переход в состояние ' + nextState)
             finally:
                 STATE = nextState
         elif STATE == 'IDLE':
             nextState = 'IDLE'
             try:
-                nextState = dronekit.IDLE(log_and_messages)
+                nextState = dronekit.IDLE(lam)
             except BaseException:
-                log_and_messages.deb_pr_tel('Произошла ошибка:\n' + BaseException.message + '\nв состоянии ' + STATE + ', переход в состояние ' + nextState)
+                lam.deb_pr_tel('Произошла ошибка:\n' + BaseException.message + '\nв состоянии ' + STATE + ', переход в состояние ' + nextState)
             finally:
                 STATE = nextState
         elif STATE == 'TAKEOFF':
             nextState = 'TAKEOFF'
             try:
-                nextState = dronekit.TAKEOFF(log_and_messages)
+                nextState = dronekit.TAKEOFF(lam)
             except BaseException:
-                log_and_messages.deb_pr_tel(
+                lam.deb_pr_tel(
                     'Произошла ошибка:\n' + BaseException.message + '\nв состоянии ' + STATE + ', переход в состояние ' + nextState)
             finally:
                 STATE = nextState
         elif STATE == 'GUIDED':
             nextState = 'GUIDED'
             try:
-                nextState = dronekit.GUIDED(log_and_messages)
+                nextState = dronekit.GUIDED(lam)
             except BaseException:
-                log_and_messages.deb_pr_tel(
+                lam.deb_pr_tel(
                     'Произошла ошибка:\n' + BaseException.message + '\nв состоянии ' + STATE + ', переход в состояние ' + nextState)
             finally:
                 STATE = nextState
         elif STATE == 'RTL':
             nextState = 'RTL'
             try:
-                nextState = dronekit.RTL(log_and_messages)
+                nextState = dronekit.RTL(lam)
             except BaseException:
-                log_and_messages.deb_pr_tel(
+                lam.deb_pr_tel(
                     'Произошла ошибка:\n' + BaseException.message + '\nв состоянии ' + STATE + ', переход в состояние ' + nextState)
             finally:
                 STATE = nextState
         elif STATE == 'AUTO':
             nextState = 'AUTO'
             try:
-                nextState = dronekit.AUTO(log_and_messages)
+                nextState = dronekit.AUTO(lam)
             except BaseException:
-                log_and_messages.deb_pr_tel(
+                lam.deb_pr_tel(
                     'Произошла ошибка:\n' + BaseException.message + '\nв состоянии ' + STATE + ', переход в состояние ' + nextState)
             finally:
                 STATE = nextState
         elif STATE == 'LAND':
             nextState = 'LAND'
             try:
-                nextState = dronekit.LAND(log_and_messages)
+                nextState = dronekit.LAND(lam)
             except BaseException:
-                log_and_messages.deb_pr_tel(
+                lam.deb_pr_tel(
                     'Произошла ошибка:\n' + BaseException.message + '\nв состоянии ' + STATE + ', переход в состояние ' + nextState)
             finally:
                 STATE = nextState
         elif STATE == 'GOTO':
             nextState = 'GOTO'
             try:
-                nextState = dronekit.GOTO(log_and_messages)
+                nextState = dronekit.GOTO(lam)
             except BaseException:
-                log_and_messages.deb_pr_tel(
+                lam.deb_pr_tel(
                     'Произошла ошибка:\n' + BaseException.message + '\nв состоянии ' + STATE + ', переход в состояние ' + nextState)
             finally:
                 STATE = nextState
         else:
             nextState = 'IDLE'
-            log_and_messages.deb_pr_tel('Произошла ошибка: не существует состояния ' + STATE + ', переход в состояние ' + nextState)
+            lam.deb_pr_tel('Произошла ошибка: не существует состояния ' + STATE + ', переход в состояние ' + nextState)
             STATE = nextState
         time.sleep(3) # для обеспечения задержки в 3 сек в случае возникновения повторяющейся ошибки
 finally:
