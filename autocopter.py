949014a802d7c76664fc3975082d756359a1dabd
Smirnov Artem <urpylka@gmail.com>
2017-06-16 16:19:43 +0400

    новая структура

--- autocopter.py
+++ autocopter.py
@@ -24,10 +24,8 @@ try:
                 # ===========================================================================================================
                 # START TELEGRAM BOT
                 #TOKEN = sys.argv[1]  # get token from command-line
-                pmName = input('Enter module name:')
-                pm = __import__('/home/pi/autocopter/telegrambot.token')
-                print(dir(pm))
-                TOKEN = dir(pm)
+                f = open('telegrambot.token', 'r')
+                TOKEN = f.readline()
                 bot = telepot.Bot(TOKEN)
                 # ==========================================================================================================
                 lam = log_and_messages(bot, MY_CHAT_ID, DEBUG)
