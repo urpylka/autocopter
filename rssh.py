1081aec13669c8397418ffefabaf70b2281c20bc
Smirnov Artem <urpylka@gmail.com>
2017-06-16 17:51:57 +0400

    новая структура

--- rssh.py
+++ rssh.py
@@ -9,6 +9,7 @@ print 'internet'
 # https://ru.stackoverflow.com/questions/225896/%D0%97%D0%B0%D0%BF%D1%83%D1%81%D0%BA-bash-%D0%B8%D0%B7-%D1%81%D0%BA%D1%80%D0%B8%D0%BF%D1%82%D0%B0-python-2-7-3
 import subprocess
 try:
+    # https://pythonworld.ru/moduli/modul-subprocess.html
     p = subprocess.Popen(['/home/pi/autocopter/build/rssh.sh'], subprocess.PIPE)
     print 'smirart.ru:2202'
     p2 = subprocess.Popen(['/home/pi/autocopter/build/rssh2.sh'], subprocess.PIPE)
@@ -26,5 +27,5 @@ try:
         import time
         time.sleep(10)
 finally:
-    p.close()
-    p2.close()
+    p.kill()
+    p2.kill()
