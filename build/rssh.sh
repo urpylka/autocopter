2a57f5f0034fa05974fec2cd209ee6031ff173bc
Artem Smirnov <urpylka@gmail.com>
2018-08-06 22:45:27 +0300

    Remove rssh

--- build/rssh.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-ssh -N -R 2202:localhost:22 -i /home/pi/.ssh/id_rsa urpylka@smirart.ru -p32122
--- build/rssh2.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-ssh -N -R 5760:localhost:5760 -i /home/pi/.ssh/id_rsa urpylka@smirart.ru -p32122
-#ssh -q -N -R 5760:localhost:5760 -i /home/pi/.ssh/id_rsa urpylka@smirart.ru -p32122
